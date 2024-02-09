0a6b4f3110a2db8971b15da9c5c8fa9d9b107fa7
99darwin <nicksapsales@gmail.com>
2024-02-09 08:24:26 -0800

    update ipfs

--- src/app/api/frame/route.ts
+++ src/app/api/frame/route.ts
@@ -11,7 +11,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
       return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame 7</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmTj8By4hPzXuJiczKjv49x5QCGfG2FFt7QexXqgdT2ap8/7.png" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmRp3YMQFLwdP1LZdsMfbMrBJqfhcXoKAb6ZYz5p38FHZV/7.png" />
     <meta property="fc:frame:button:1" content="Visit diviswap.com" />
     <meta property="fc:frame:button:1:action" content="post_redirect" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
@@ -20,7 +20,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame ${id}</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmTj8By4hPzXuJiczKjv49x5QCGfG2FFt7QexXqgdT2ap8/${id}.png" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmRp3YMQFLwdP1LZdsMfbMrBJqfhcXoKAb6ZYz5p38FHZV/${id}.png" />
     <meta property="fc:frame:button:1" content="Left door" />
     <meta property="fc:frame:button:1:action" content="post" />
     <meta property="fc:frame:button:2" content="Right door" />
