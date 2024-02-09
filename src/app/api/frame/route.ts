e5b03ef84700af0c20bf2c71c13fb78f315797b8
99darwin <nicksapsales@gmail.com>
2024-02-08 21:33:05 -0800

    update begin endpoint

--- src/app/api/frame/route.ts
+++ src/app/api/frame/route.ts
@@ -25,7 +25,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     <meta property="fc:frame:button:1:action" content="post" />
     <meta property="fc:frame:button:2" content="Right door" />
     <meta property="fc:frame:button:2:action" content="post" />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/begin" />
   </head></html>`);
   }
 }
